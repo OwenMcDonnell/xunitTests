b0f74c1c84827b932204569991a64d4819b7308a
britzkopf <britzkopf@users.noreply.github.com>
2018-02-18 09:36:20 +0800

    initialized repo

---
+++ xunitTestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace xunitTests
+{
+    public class xunitTestClass
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(1,1);
+        }
+
+    }
+}
